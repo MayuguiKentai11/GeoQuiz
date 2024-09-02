76537488d387fdbc6a4d6899f8513b6d1eecda3f
Mauricio <mauriciorojasanchez@gmail.com>
2024-09-02 18:06:29 -0500

    feat: configured main activity for application.

--- app/src/main/java/com/example/app_sem3_s1_geoquizwx64/MainActivity.kt
+++ app/src/main/java/com/example/app_sem3_s1_geoquizwx64/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.app_sem3_s1_geoquizwx64
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+const val EXTRA_MESS = "com.example.app_sem3_s1_geoquizwx64.MESSAGE"
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,5 +23,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun sendMessageLevel1(view : View){
+        // Link the components to UI with logic
 
+        val intent = Intent(this, Level1Activity::class.java)
+
+        startActivity(intent)
+    }
+
+    fun sendMessageLevel2(view : View){
+        // Link the components to UI with logic
+
+        val intent = Intent(this, Level2Activity::class.java)
+
+        startActivity(intent)
+    }
+
+    fun sendMessageLevel3(view : View){
+        // Link the components to UI with logic
+
+        val intent = Intent(this, Level3Activity::class.java)
+
+        startActivity(intent)
+    }
 }
